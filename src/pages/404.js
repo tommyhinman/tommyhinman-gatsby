23d7218485d31a19234efede456d38805af1f979
tommyhinman <tommyhinman@gmail.com>
2020-08-23 10:26:30 -0700

    Hopefully prevent flash of 404 on first render of app page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,18 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h3>Not Found!</h3>
-    <p>You hit a route that doesn&#39;t exist!</p>
-  </Layout>
-)
+const browser = typeof window !== "undefined" && window;
+
+const NotFoundPage = () => {
+  return (
+    browser && (
+      <Layout>
+        <SEO title="404: Not found" />
+        <h3>Not Found!</h3>
+        <p>You hit a route that doesn&#39;t exist!</p>
+      </Layout>
+    )
+  );
+}
 
 export default NotFoundPage
