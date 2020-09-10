f2e02268f98a0e9cb68998b070fdf8335609f6ab
tommyhinman <tommyhinman@gmail.com>
2020-09-10 11:14:50 -0700

    Some cleanup on the albums page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,29 +3,39 @@ import Layout from "../components/layout"
 import { Helmet } from "react-helmet"
 import useDataApi from '../components/dataApi.js'
 import axios from "axios";
+import moment from "moment-timezone";
 
 export default function Projects() {
 
   const [{data, isLoading, isError}, searchUrl, doFetch] = useDataApi(
-    "https://7j2apoxwhf.execute-api.us-west-2.amazonaws.com/Prod/latestRequest?format=json",
-    []
+    // "https://7j2apoxwhf.execute-api.us-west-2.amazonaws.com/Prod/latestRequest?format=json",
+    "http://localhost:3000/latestRequest?format=json",
+    {
+      "albums": [],
+      "request": {"requestId": ""}
+    }
   );
 
   const Artist = ({artistData}) => {
-    console.log("hi!");
     return (
-      <div>
-        <b>{artistData.artistName}</b>
-        <ul>
+        <li>
           {artistData.albums.map(album => (
             <div>
-              <a href={album.albumUri}>{album.albumName} ({album.albumType})</a>
+              {artistData.artistName} - <a href={album.albumUri}>{album.albumName} ({album.albumType})</a>
             </div>
           ))}
-        </ul>
-      </div>
+        </li>
     )
   }
+
+  const RequestInfo = ({request}) => {
+    const formattedDate = moment(request.requestTime).tz('America/Los_Angeles').format("LLL");
+    return (
+      <div>Latest Scan Request, on {formattedDate}, with ID {request.requestId}</div>
+    )
+  }
+
+
 
   return (
     <div>
@@ -43,9 +53,12 @@ export default function Projects() {
             <div>Loading!</div>
           ) : (
             <div>
-              {data.map(artist => (
-                <Artist artistData={artist} />
-              ))}
+              <RequestInfo request={data.request}/>
+              <ul>
+                {data.albums.map(artist => (
+                  <Artist artistData={artist} />
+                ))}
+              </ul>
             </div>
           )
         }
