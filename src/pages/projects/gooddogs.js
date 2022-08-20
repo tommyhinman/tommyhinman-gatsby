b4865026b9827c622e581f46020791c14e92e905
tommyhinman <tommyhinman@gmail.com>
2022-08-20 12:25:26 -0700

    Adding image modal to dogs

--- src/pages/projects/gooddogs.js
+++ src/pages/projects/gooddogs.js
@@ -1,40 +1,88 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Helmet } from "react-helmet"
 import Layout from "../../components/layout"
 import { getDogs } from "../../data/dogs"
+import * as styles from "./gooddogs.module.css"
 
-const Dog = ({ dogData }) => {
+const isMobileWidth = width => {
+  return width <= 768
+}
+
+let modalEnabled = false
+
+const Dog = ({ dogData, onImageClick }) => {
   return (
-    <>
-      <div className=" container">
-        <span class="subtitle is-4">{dogData.title}</span>
-        <figure className="image mt-2">
-          <img src={dogData.imageUrl} />
-        </figure>
-      </div>
-    </>
+    <div className=" container">
+      <span className="subtitle is-4">{dogData.title}</span>
+      <img
+        src={dogData.imageUrl}
+        onClick={() => onImageClick(dogData.imageUrl)}
+      />
+    </div>
   )
 }
 
 export default function Gooddogs() {
   const dogs = getDogs()
+
+  const [isModalEnabled, setIsModalEnabled] = useState(false)
+  const [modalImageUrl, setModalImageUrl] = useState("")
+  const [isMobile, setIsMobile] = useState(isMobileWidth(window.innerWidth))
+
+  const openModal = url => {
+    if (!isMobile) {
+      setIsModalEnabled(true)
+      setModalImageUrl(url)
+    }
+  }
+
+  const closeModal = url => {
+    setIsModalEnabled(false)
+  }
+
+  function handleWindowSizeChange() {
+    setIsMobile(isMobileWidth(window.innerWidth))
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange)
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange)
+    }
+  }, [])
+
   return (
     <>
       <Helmet>
         <meta charSet="utf-8" />
         <title>good dogs | tommyhinman</title>
+        <html className={isModalEnabled ? styles.scrollingLocked : ""}></html>
+        <body className={isModalEnabled ? styles.scrollingLocked : ""}></body>
       </Helmet>
       <Layout>
         <h1 className="title is-size-2-desktop is-size-3-mobile">Good Dogs</h1>
         <div className=" mt-0 mb-5">
           {dogs.map((dog, index, arr) => (
             <>
-              <Dog dogData={dog} key={index} />
+              <Dog dogData={dog} key={index} onImageClick={openModal} />
               {index === arr.length - 1 ? "" : <hr />}
             </>
           ))}
         </div>
       </Layout>
+      <div className={isModalEnabled ? "modal is-active" : "modal"}>
+        <div className="modal-background" onClick={closeModal}></div>
+        <div className="modal-content" onClick={closeModal}>
+          <p className={"image is-large " + styles.modalImage}>
+            <img src={modalImageUrl} alt="" />
+          </p>
+        </div>
+        <button
+          className="modal-close is-large"
+          aria-label="close"
+          onClick={closeModal}
+        ></button>
+      </div>
     </>
   )
 }
