dee3cc84b7b8e3a6323b8547590d0055bdddd3e1
tommyhinman <tommyhinman@gmail.com>
2022-08-20 11:50:47 -0700

    Switching to cloudfront urls

--- src/data/dogs.js
+++ src/data/dogs.js
@@ -2,53 +2,43 @@ export function getDogs() {
   return [
     {
       title: "Trafic (1971)",
-      imageUrl:
-        "https://tommyhinman-dogs.s3.us-west-2.amazonaws.com/dogs/trafic.png",
+      imageUrl: "https://d2vxchc6g5met5.cloudfront.net/dogs/trafic.png",
     },
     {
       title: "Serpico (1973)",
-      imageUrl:
-        "https://tommyhinman-dogs.s3.us-west-2.amazonaws.com/dogs/serpico.png",
+      imageUrl: "https://d2vxchc6g5met5.cloudfront.net/dogs/serpico.png",
     },
     {
       title: "Blow Up (1966)",
-      imageUrl:
-        "https://tommyhinman-dogs.s3.us-west-2.amazonaws.com/dogs/blowup.png",
+      imageUrl: "https://d2vxchc6g5met5.cloudfront.net/dogs/blowup.png",
     },
     {
       title: "Touch of Evil (1958)",
-      imageUrl:
-        "https://tommyhinman-dogs.s3.us-west-2.amazonaws.com/dogs/touchofevil.png",
+      imageUrl: "https://d2vxchc6g5met5.cloudfront.net/dogs/touchofevil.png",
     },
     {
       title: "Hiroshima Mon Amour (1959)",
-      imageUrl:
-        "https://tommyhinman-dogs.s3.us-west-2.amazonaws.com/dogs/hiroshima.png",
+      imageUrl: "https://d2vxchc6g5met5.cloudfront.net/dogs/hiroshima.png",
     },
     {
       title: "Barry Lyndon (1975)",
-      imageUrl:
-        "https://tommyhinman-dogs.s3.us-west-2.amazonaws.com/dogs/barrylyndon.png",
+      imageUrl: "https://d2vxchc6g5met5.cloudfront.net/dogs/barrylyndon.png",
     },
     {
       title: "Barking Dogs Never Bite (2000)",
-      imageUrl:
-        "https://tommyhinman-dogs.s3.us-west-2.amazonaws.com/dogs/barking.png",
+      imageUrl: "https://d2vxchc6g5met5.cloudfront.net/dogs/barking.png",
     },
     {
       title: "Wonder Boys (2000)",
-      imageUrl:
-        "https://tommyhinman-dogs.s3.us-west-2.amazonaws.com/dogs/wonderboys.png",
+      imageUrl: "https://d2vxchc6g5met5.cloudfront.net/dogs/wonderboys.png",
     },
     {
       title: "Pig (2021)",
-      imageUrl:
-        "https://tommyhinman-dogs.s3.us-west-2.amazonaws.com/dogs/pig.png",
+      imageUrl: "https://d2vxchc6g5met5.cloudfront.net/dogs/pig.png",
     },
     {
       title: "Inside Llewyn Davis (2013)",
-      imageUrl:
-        "https://tommyhinman-dogs.s3.us-west-2.amazonaws.com/dogs/llewyn.png",
+      imageUrl: "https://d2vxchc6g5met5.cloudfront.net/dogs/llewyn.png",
     },
   ]
 }
