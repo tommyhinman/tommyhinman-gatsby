79368779586fb31aea170d6601283322759237a3
tommyhinman <tommyhinman@gmail.com>
2022-12-19 22:41:47 -0800

    Initial add of music library

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,34 +4,44 @@ import "../mystyles.scss"
 import * as styles from "./layout.module.scss"
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
+import classNames from "classnames"
 
-
-export const Layout = ({ children }) => {
+export const Layout = ({ children, width = "normal" }) => {
   return (
     <>
       {/* Only add the cloudflare analytics tag in production, since it checks CORS. */}
       {process.env.NODE_ENV != "development" && (
         <Helmet>
-          <script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon="{&quot;token&quot;: &quot;3aa680a9c3c44e04a8a33c6d45116289&quot;}"></script>
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "3aa680a9c3c44e04a8a33c6d45116289"}'
+          ></script>
         </Helmet>
       )}
-      <div className={styles.parentContainer} >
+      <div className={styles.parentContainer}>
         <Navbar />
         <div className="section mt-0 pt-5 mx-1 is-hidden-tablet">
           <div className="columns is-centered">
-            <div className="column is-8-desktop is-full-mobile">
-              {children}
-            </div>
+            <div className="column is-8-desktop is-full-mobile">{children}</div>
           </div>
         </div>
         <div className="mt-6 mx-6 is-hidden-mobile">
           <div className="columns is-centered">
-            <div className="column is-8-desktop is-full-mobile">
+            <div
+              className={classNames(
+                "column",
+                { "is-8-desktop": width === "normal" },
+                { "is-10-desktop": width === "wide" },
+                "is-full-mobile"
+              )}
+            >
               {children}
             </div>
           </div>
         </div>
-        <div className={styles.spaceFiller} /> {/*Flexible Invisible Space Filler!*/}
+        <div className={styles.spaceFiller} />{" "}
+        {/*Flexible Invisible Space Filler!*/}
         <Footer />
       </div>
     </>
---
+++ src/pages/projects/music-library.js
@@ -0,0 +1,194 @@
+import React, { useState, useEffect } from "react"
+import Layout from "../../components/layout"
+import * as styles from "./music-library.module.css"
+import { Helmet } from "react-helmet"
+import classNames from "classnames"
+import useDataApi from "../../components/dataApi"
+import { FaSpotify } from "react-icons/fa"
+import { HiExternalLink } from "react-icons/hi"
+
+const LibraryItem = ({ index, item }) => {
+  const {
+    primaryText,
+    secondaryText,
+    imageLink = "https://tommyhinman-albums.s3-us-west-2.amazonaws.com/512/corrupted-garten-der-unbewusstheit.jpg",
+    spotifyLink,
+    externalLink,
+  } = item
+  return (
+    <>
+      <a
+        className={classNames("box", "is-clickable", styles.itemBox)}
+        href={externalLink}
+        target="_blank"
+        key={"item-" + index}
+      >
+        <figure className="image is-100x100">
+          <img src={imageLink} />
+        </figure>
+        <div>
+          <div className="mt-2 is-size-5">{primaryText}</div>
+          <div className="is-size-6">{secondaryText}</div>
+        </div>
+        <div
+          className={classNames(
+            "columns",
+            "is-centered",
+            "mt-2",
+            styles.itemButtonGroup
+          )}
+        >
+          <div className="field has-addons">
+            <p className="control">
+              <a href={spotifyLink}>
+                <button className="button" disabled={!spotifyLink}>
+                  <span className="icon is-small">
+                    <FaSpotify />
+                  </span>
+                </button>
+              </a>
+            </p>
+            <p className="control">
+              <a href={externalLink} target="_blank">
+                <button className="button" disabled={!externalLink}>
+                  <span className="icon is-small">
+                    <HiExternalLink />
+                  </span>
+                </button>
+              </a>
+            </p>
+          </div>
+        </div>
+      </a>
+    </>
+  )
+}
+
+// const dataUrl =
+//   "https://tommy-music-library.s3.us-west-2.amazonaws.com/music-library-data.json"
+const dataUrl =
+  "https://mqze13mg7g.execute-api.us-west-2.amazonaws.com/Prod/libraryItems"
+
+const getItemData = () => {
+  const data = [
+    {
+      primaryText: "Craven Faults",
+      secondaryText: "Erratics & Unconformities",
+      imageLink:
+        "https://cdn2.albumoftheyear.org/750x/album/190023-erratics-unconformities.jpg",
+      externalLink:
+        "https://open.spotify.com/album/2BsxDJWcmm9iFH1BFdXO6V?si=yYI0dJvfQwOP8Ax0-bjgwA",
+      spotifyLink:
+        "spotify://album/2BsxDJWcmm9iFH1BFdXO6V?si=yYI0dJvfQwOP8Ax0-bjgwA",
+      tags: ["instrumental"],
+    },
+    {
+      primaryText: "album 2",
+      secondaryText: "subtext",
+      tags: ["party"],
+    },
+    {
+      primaryText: "album 3",
+      secondaryText: "subtext",
+    },
+    {
+      primaryText: "album 4",
+      secondaryText: "subtext",
+    },
+    {
+      primaryText: "album 500",
+      secondaryText: "subtext",
+    },
+    {
+      primaryText: "album 6",
+    },
+  ]
+
+  return data
+}
+
+const TagButton = ({ tagName, isSelected, onClick }) => {
+  const buttonClasses = classNames({
+    button: true,
+    "is-success": isSelected,
+  })
+
+  return (
+    <button className={buttonClasses} onClick={onClick}>
+      {tagName}
+    </button>
+  )
+}
+
+export default function MusicLibrary() {
+  // const items = getItemData()
+  const [libraryDataQuery, libraryDataUrl, requestFetch] = useDataApi(
+    dataUrl,
+    []
+  )
+
+  const [tags, setTags] = useState({ instrumental: false, party: false })
+  const [stateTest, setStateTest] = useState(1)
+
+  const filterData = data => {
+    const enabledTags = Object.keys(tags).filter(currentTag => tags[currentTag])
+    const filteredData = data.filter(item => {
+      if (enabledTags.length > 0) {
+        return (
+          item.tags &&
+          item.tags.filter(tag => enabledTags.includes(tag)).length > 0
+        )
+      } else {
+        return true
+      }
+    })
+    return filteredData
+  }
+  const items = filterData(libraryDataQuery.data)
+  console.log(JSON.stringify(items))
+
+  const tagButtonClicked = tagName => {
+    setTags({ ...tags, [tagName]: !tags[tagName] })
+  }
+
+  const tagNames = ["instrumental", "party"]
+
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>music library | tommyhinman</title>
+      </Helmet>
+      <Layout width="wide">
+        <h1 className="title">Music Library</h1>
+        <div className="buttons">
+          {tagNames.map(tag => (
+            <TagButton
+              tagName={tag}
+              isSelected={tags[tag]}
+              onClick={() => tagButtonClicked(tag)}
+            />
+          ))}
+        </div>
+        {!libraryDataQuery.isLoading && items != null && (
+          <>
+            <div className="columns is-multiline mb-5">
+              {items.map((item, index) => (
+                <div
+                  key={"col-" + index}
+                  className={classNames(
+                    "column",
+                    "is-one-fifth",
+                    styles.itemColumn
+                  )}
+                >
+                  <LibraryItem index={index} item={item} />
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </Layout>
+    </>
+  )
+}
