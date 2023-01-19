eed3fae2d684f63076740d8468147d49ea7f9aaa
tommyhinman <tommyhinman@gmail.com>
2023-01-18 20:51:25 -0800

    Adding best of 2022 albums

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,13 +3,16 @@ import Link from "gatsby-link"
 import "../mystyles.scss"
 
 class Footer extends Component {
-
   render() {
     return (
       <footer className="footer sticky">
         <div className="content has-text-centered">
           <p className="is-size-7">
-           2022 - tommy hinman. source at <a href="https://github.com/tommyhinman/tommyhinman-gatsby">github</a>.
+            2023 - tommy hinman. source at{" "}
+            <a href="https://github.com/tommyhinman/tommyhinman-gatsby">
+              github
+            </a>
+            .
           </p>
         </div>
       </footer>
