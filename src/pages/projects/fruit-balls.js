e92188127913c3d3883e22ef4c677e70f1caaddc
tommyhinman <tommyhinman@gmail.com>
2024-01-06 19:32:32 -0800

    Fix build error

--- src/pages/projects/fruit-balls.js
+++ src/pages/projects/fruit-balls.js
@@ -4,10 +4,6 @@ import Game from "./fruit-balls/game"
 import "../../mystyles.scss"
 
 export default function FruitBalls() {
-  const width = Math.min(window.innerWidth, 600)
-  const height = Math.min(window.innerHeight, 800)
-
-  const centerGame = width > 600
   return (
     <>
       <Helmet>
